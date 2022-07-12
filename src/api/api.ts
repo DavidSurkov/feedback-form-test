e7e8a7766002e5686a63df8c7179d104e7967c59
David Surkov <surkovdavid@gmail.com>
2022-07-12 20:34:58 +0300

    url has been changed

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const api = {
   send(form: FormType) {
-    return axios.post('http://localhost:9000', form, {
+    return axios.post('https://form-data-base.herokuapp.com/form', form, {
       method: 'POST',
       headers: {
         'Access-Control-Allow-Origin': '*',
